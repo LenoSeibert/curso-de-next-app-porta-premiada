b0f87852b404309cea66e94309c258a3daa377bc
Lenoir Seibert <siwebbr@gmail.com>
2021-09-26 20:00:55 -0300

    porta

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,8 +4,9 @@ import Porta from "../components/Porta";
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ display: 'flex' }}>
 
+      <Porta />
       <Porta />
     </div >
   )
